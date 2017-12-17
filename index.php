a58e7cda157f77e84f3b4571bb30e99a36a0c9a4
Rikito Takahashi <4lphamath@gmail.com>
2017-12-17 22:39:31 +0900

    Fixed index.php and style

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
     <head>
         <meta charset="utf-8">
         <title>京大スマブラ会</title>
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
         <div class="title">
