4bd8a9ae3d45cd2f7d2bb29b3c0e608febd720e5
Rikito Takahashi <4lphamath@gmail.com>
2017-12-17 22:21:13 +0900

    Moved index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>京大スマブラ会</title>
+    </head>
+    <body>
+        <div class="title">
+            <h1>京大スマブラ会</h1>
+            京都大学唯一のスマブラサークル「京大スマブラ会」のサイトです。
+        </div>
+        <div class="menu">
+            <ul>
+                <li>京大スマブラ会とは？</li>
+                <li>メンバー紹介</li>
+                <li>イベント情報</li>
+            </ul>
+        </div>
+    </body>
+</html>
